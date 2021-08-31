7cea937ffaf16bd1bc215ad2dc184400c264f055
David Herrera <biggiegun@gmail.com>
2021-08-31 16:58:24 -0500

    pintando producto en detalle

--- js/index2.js
+++ js/index2.js
@@ -10,11 +10,30 @@ document.addEventListener('DOMContentLoaded', async (e)=>{
     e.preventDefault()
     console.log(dataBuscar)
     
-    let resp = await fetch(`http://localhost:4001/${categoria}`)
+    let resp = await fetch(`http://localhost:3001/${categoria}`)
     let data = await resp.json();
     console.log(data)
     let buscar = data.find(buscar=>buscar.id === dataBuscar)
 
     console.log(buscar)
+
+    const {inventario, nombrePrenda, precio, thumbnailUrl} = buscar;
+    
+    let pintar = '';
+
+    pintar = `
+    <div id="items">
+        <div id="imagenesGrandes">
+            <img src="${thumbnailUrl.imagen1}" alt="imagen1">
+            <img src="${thumbnailUrl.imagen2}" alt="imagen2">
+            <img src="${thumbnailUrl.imagen3}" alt="imagen3">
+        </div>
+        <div id="infoProducto">
+            <span>${nombrePrenda}<br></span>
+            <span>${precio}<br></span>
+            <span>${inventario}<br></span>
+        </div>
+    </div>`
+document.querySelector('#principal').innerHTML = pintar;
 })
 
