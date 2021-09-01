d457ef044a0484cf17aa1966f69fd07a22d580bb
Ilan Diaz <idiazjimenez06@gmail.com>
2021-08-31 23:01:21 -0500

    UPDATE avances

--- js/index2.js
+++ js/index2.js
@@ -2,6 +2,15 @@ const items = document.getElementById('items');
 const fragment = document.createDocumentFragment()
 const dataBuscar =  JSON.parse(localStorage.getItem('dataBuscar'))
 const categoria =  JSON.parse(localStorage.getItem('categoria'))
+const contTotal = document.getElementById('contenedorTotal')
+const sidebarCarrito = document.getElementById('sidebarCarrito')
+const btnSidebarCerrar = document.getElementById('btn-cierre')
+const btnSidebarRemove = document.getElementById('btn-remove')
+const btnSidebarCheckout = document.getElementById('btn-checkout')
+let cantCompra = document.getElementById('cantCompra')
+const btnAgregar = document.getElementById('btn-agregar')
+const btnComprar = document.getElementById('btn-comprar')
+const body = document.getElementById('cuerpo')
 
 
 
@@ -10,7 +19,7 @@ document.addEventListener('DOMContentLoaded', async (e)=>{
     e.preventDefault()
     console.log(dataBuscar)
     
-    let resp = await fetch(`http://localhost:3001/${categoria}`)
+    let resp = await fetch(`http://localhost:4001/${categoria}`)
     let data = await resp.json();
     console.log(data)
     let buscar = data.find(buscar=>buscar.id === dataBuscar)
@@ -21,19 +30,53 @@ document.addEventListener('DOMContentLoaded', async (e)=>{
     
     let pintar = '';
 
-    pintar = `
-    <div id="items">
-        <div id="imagenesGrandes">
-            <img src="${thumbnailUrl.imagen1}" alt="imagen1">
-            <img src="${thumbnailUrl.imagen2}" alt="imagen2">
-            <img src="${thumbnailUrl.imagen3}" alt="imagen3">
-        </div>
-        <div id="infoProducto">
-            <span>${nombrePrenda}<br></span>
-            <span>${precio.toFixed(2)}<br></span>
-            <span>${inventario}<br></span>
-        </div>
-    </div>`
-document.querySelector('#principal').innerHTML = pintar;
+    document.querySelectorAll('.img-peq')[0].setAttribute('src', thumbnailUrl.imagen1)
+    document.querySelectorAll('.img-peq')[1].setAttribute('src', thumbnailUrl.imagen2)
+    document.querySelectorAll('.img-peq')[2].setAttribute('src', thumbnailUrl.imagen3)
+    document.querySelectorAll('.img-grd')[0].setAttribute('src', thumbnailUrl.imagen1)
+    document.querySelectorAll('.img-grd')[1].setAttribute('src', thumbnailUrl.imagen2)
+    document.querySelectorAll('.img-grd')[2].setAttribute('src', thumbnailUrl.imagen3)
+    document.getElementById('nomProd').textContent = nombrePrenda
+    document.getElementById('precioProd').textContent = precio.toFixed(2)
+    
 })
 
+btnAgregar.addEventListener('click',e=>{
+    console.log('clickAgrega')
+    agregarSidebar()
+})
+btnComprar.addEventListener('click',e=>{
+    console.log('clickCompra')
+})
+
+
+const agregarSidebar = () =>{
+    sidebarCarrito.style.display = 'block'
+    sidebarCarrito.style.opacity = '1'
+    contTotal.style.overflow = 'hidden'
+    contTotal.style.background = '#585858'
+    contTotal.style.opacity = '.5'    
+}
+
+btnSidebarCerrar.addEventListener('click',e=>{
+    e.preventDefault()
+    sidebarCarrito.style.display = 'none'
+    contTotal.style.overflow = ''
+    contTotal.style.background =''
+    contTotal.style.opacity = '' 
+})
+
+
+btnSidebarRemove.addEventListener('click', e=>{
+    e.preventDefault()
+    console.log(Number(cantCompra.textContent))
+    console.log(Number(cantCompra.textContent)-1)
+
+    cantCompra.textContent = Number(cantCompra.textContent)-1
+
+
+})
+btnSidebarCheckout.addEventListener('click', e=>{
+    e.preventDefault()
+    
+})
